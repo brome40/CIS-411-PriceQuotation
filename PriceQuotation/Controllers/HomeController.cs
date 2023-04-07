3d6ff9e4d0a15eb474bb027cdb940be6b052a258
brome40 <85357500+brome40@users.noreply.github.com>
2023-04-06 20:51:25 -0400

    Add project files

---
+++ PriceQuotation/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuotation.Models;
+using System.Diagnostics;
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Discount = 0;
+            ViewBag.TotalCost = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Discount = model.CalculateDiscount();
+                ViewBag.TotalCost = model.CalculateTotalCost();
+            }
+            else
+            {
+                ViewBag.Discount = 0;
+                ViewBag.TotalCost = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ PriceQuotation/Models/PriceQuotationModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter a sales price.")]
+        [Range(0.0, Int32.MaxValue, ErrorMessage =
+               "Sales price must be greater than 0.")]
+        public decimal? SalesPrice { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount rate.")]
+        [Range(0.0, 100.0, ErrorMessage =
+               "Discount rate must be between 0 and 100.")]
+        public decimal? DiscountRate { get; set; }
+
+        public decimal? CalculateDiscount()
+        {
+            decimal? discount = SalesPrice * (DiscountRate / 100.0m);
+            return discount;
+        }
+
+        public decimal? CalculateTotalCost()
+        {
+            return SalesPrice * CalculateDiscount();
+        }
+    }
+}
